0213edd8dea0910939a367c5ce5dae0a002cb398
rcastroavendano <131787241+rcastroavendano@users.noreply.github.com>
2023-07-03 09:45:20 -0400

    ok

--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hola, Mundo, desde Espagna.");
+    expect(hello.hello()).toEqual("Hola Mundo");
   });
 });
